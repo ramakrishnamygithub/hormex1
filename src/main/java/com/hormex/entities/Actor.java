7f1ca322434bfa2a0ce0c2dab74856bd7b44108a
ramakrishna <ramakrishna@Rklaptop>
2021-03-28 19:50:43 +0530

    First commit added Hibernate first project

---
+++ src/main/java/com/hormex/entities/Actor.java
@@ -0,0 +1,91 @@
+package com.hormex.entities;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Actor implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7893362889835304524L;
+	
+	protected int actorId;
+	protected String firstName;
+	protected String lastName;
+	protected Date lastUpdate;
+	public int getActorId() {
+		return actorId;
+	}
+	public void setActorId(int actorId) {
+		this.actorId = actorId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + actorId;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((lastUpdate == null) ? 0 : lastUpdate.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Actor other = (Actor) obj;
+		if (actorId != other.actorId)
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (lastUpdate == null) {
+			if (other.lastUpdate != null)
+				return false;
+		} else if (!lastUpdate.equals(other.lastUpdate))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Actor [actorId=" + actorId + ", firstName=" + firstName + ", lastName=" + lastName + ", lastUpdate="
+				+ lastUpdate + "]";
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/hormex/test/HORMEXTest.java
@@ -0,0 +1,19 @@
+package com.hormex.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hormex.entities.Actor;
+
+public class HORMEXTest {
+	public static void main(String[] args) {
+		Configuration configuration=new Configuration().configure();
+		SessionFactory sessionFactory=configuration.buildSessionFactory();
+		       Session session=sessionFactory.openSession();
+		    // Account account=  session.get(Account.class, 1);
+		       Actor  actor=  session.get(Actor.class, 1);
+		     System.out.println(actor);
+	}
+
+}
